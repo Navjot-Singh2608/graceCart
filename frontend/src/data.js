99a1a3976ced76df47d8aca129a4e86d5de4e0f4
Navjot Singh <navjot2608@gmail.com>
2020-11-06 23:44:15 -0500

    changes

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      image: "/images/product-1.png",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product"
+    },
+    {
+      _id: "2",
+      name: "Adidas Slim Shirt",
+      image: "/images/product-1.png",
+      price: 100,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality product"
+    },
+    {
+      _id: "3",
+      name: "Lacoste Slim Shirt",
+      image: "/images/product-1.png",
+      price: 220,
+      brand: "Nike",
+      rating: 4.8,
+      numReviews: 17,
+      description: "high quality product"
+    },
+    {
+      _id: "4",
+      name: "Puma Slim Pants",
+      image: "/images/product-1.png",
+      price: 140,
+      brand: "Nike",
+      rating: 4.2,
+      numReviews: 14,
+      description: "high quality product"
+    },
+    {
+      _id: "5",
+      name: "Adidas Slim Pant",
+      image: "/images/product-1.png",
+      price: 130,
+      brand: "Adidas",
+      rating: 4.5,
+      numReviews: 18,
+      description: "high quality product"
+    }
+  ]
+};
+
+export default data;
